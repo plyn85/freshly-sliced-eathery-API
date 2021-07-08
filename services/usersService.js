89fa6ad23933f43f2dad2ba729aaa61b18aa4882
plyn85 <plyn99@gmail.com>
2021-07-08 19:01:17 +0100

    adding validator file-connecting with user service

--- services/usersService.js
+++ services/usersService.js
@@ -1,10 +1,18 @@
 //imports
+const userDataValidation = require("../validators/userDataValidation");
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
 
 //allow the user to login
 const loginUser = async (userData) => {
-  return userData;
+  let validatedUserData;
+  //validate the user data
+  let validateUserData = userDataValidation.validateUser(userData);
+  //if it returns true
+  if (validateUserData) {
+    validatedUserData = validateUserData;
+  }
+  return validatedUserData;
 };
 
 //exports
---
+++ validators/userDataValidation.js
@@ -0,0 +1,8 @@
+//validate the user data
+
+const validateUser = (userData) => {
+  console.log(userData);
+  return true;
+};
+
+module.exports = { validateUser };
